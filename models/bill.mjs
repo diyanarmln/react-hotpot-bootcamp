1320ed6f8c8d343e0f86e27babfce429930a9ce5
diyanarmln <diyana.rmln@gmail.com>
2022-04-24 11:00:39 +0800

    created bill model

---
+++ models/bill.mjs
@@ -0,0 +1,29 @@
+export default function initBillModel(sequelize, DataTypes) {
+  return sequelize.define('bill', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    total: {
+      allowNull: false,
+      type: DataTypes.NUMERIC(18, 8),
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    // The underscored option makes Sequelize reference snake_case names in the DB.
+    underscored: true,
+  });
+}
