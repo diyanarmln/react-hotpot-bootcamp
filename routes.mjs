e4263a3ad10824a1a559e8b8aa8daf5c83894bbf
diyanarmln <diyana.rmln@gmail.com>
2022-04-24 11:46:32 +0800

    commented out unused route for now

--- routes.mjs
+++ routes.mjs
@@ -6,6 +6,8 @@ import initBillsController from './controllers/bills.mjs';
 export default function routes(app) {
   const billsController = initBillsController(db);
 
+  // app.get('bill/:id/new', billsController.show);
+
   // Root route renders Webpack-generated main.html file
   app.get('/', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
